c0096b23a96b39092ce417d3ff097a034044b883
Tejas <tejas@callandor.io>
2024-03-03 23:30:49 +0400

    Change peekError function in parser to use p.peekToken.Type instead of p.peekToken in print, keeping it consistent with tokenType print

--- parser/parser.go
+++ parser/parser.go
@@ -112,7 +112,7 @@ func (p *Parser) Errors() []string {
 
 func (p *Parser) peekError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
-		t, p.peekToken)
+		t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
 }
 
